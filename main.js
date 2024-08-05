c4053c177b9c0fa1aa8b589ba5d6a6e190c30df9
Branko Mirkovic <mirkovic_branko@yahoo.com>
2024-08-05 21:58:20 +0200

    Changed the js and some css

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 let start = document.querySelector(".karta");
 start.addEventListener("click", pokreni);
 
-
-
-let brojeviSlika = []; // Niz za čuvanje indeksa slika
-let otvoreneSlike = []; //niz za pracenje otvorenih slika
-let nizUklonjenih = []; //niz za pracenje broja uklonjenih slika
+let brojeviSlika = [];
+let otvoreneSlike = [];
+let nizUklonjenih = [];
 
 for (let i = 0; i < 5; i++) {
   brojeviSlika.push(i);
   brojeviSlika.push(i);
 }
+
 function pokreni() {
   let ispis = document.getElementById("ispis");
+  ispis.innerHTML = ''; // Resetuje ispis pre nove igre
   let brojac = 0;
-  promesajNiz(brojeviSlika); //mesa niz indexa
+  promesajNiz(brojeviSlika);
   let tajmerSlika = setInterval(function () {
-    let brSlike = brojeviSlika[brojac]; //koristi index iz niza
+    let brSlike = brojeviSlika[brojac];
     ispis.innerHTML += `<div class="card" id="${brSlike}">
-        <div class="card__face card__face--front"></div>
-        <div class="card__face card__face--back">
-        <img src="/images/${brSlike}.png"  alt="photo${brSlike}">
-        </div>
-      </div>`;
+            <div class="card__face card__face--front"></div>
+            <div class="card__face card__face--back">
+                <img src="/images/${brSlike}.png" alt="photo${brSlike}">
+            </div>
+        </div>`;
     brojac++;
     if (brojac == 10) {
       clearInterval(tajmerSlika);
@@ -30,42 +30,43 @@ function pokreni() {
     }
   }, 500);
 }
-let tajmer;
+
 function rotiraj() {
-  var cards = document.querySelectorAll('.card'); // Finds all elements with class .card
+  var cards = document.querySelectorAll('.card');
+
   cards.forEach(function (card, index) {
     card.addEventListener('click', function () {
       if (!card.classList.contains('uklonjena') && (otvoreneSlike.length < 2 || otvoreneSlike.includes(index))) {
         if (otvoreneSlike.length < 2 || otvoreneSlike.includes(index)) {
-          card.classList.toggle('is-flipped'); //Adds or removes class for rotation
+          card.classList.toggle('is-flipped');
           if (otvoreneSlike.includes(index)) {
             otvoreneSlike.splice(otvoreneSlike.indexOf(index), 1);
           } else {
-            otvoreneSlike.push(index); //adds picture in array of open pictures
+            otvoreneSlike.push(index);
           }
-          //ih if 2 pictures are open, it closes
+
           if (otvoreneSlike.length == 2) {
             setTimeout(function () {
-              if (cards[otvoreneSlike[0]].id === cards[otvoreneSlike[1]].id) {//compares 2 pictures
+              if (cards[otvoreneSlike[0]].id === cards[otvoreneSlike[1]].id) {
                 cards[otvoreneSlike[0]].style.display = "none";
                 cards[otvoreneSlike[1]].style.display = "none";
-                otvoreneSlike = []; //empties array of open pictures
+                cards[otvoreneSlike[0]].classList.add('uklonjena');
+                cards[otvoreneSlike[1]].classList.add('uklonjena');
+                otvoreneSlike = [];
               } else {
                 cards[otvoreneSlike[0]].classList.remove("is-flipped");
                 cards[otvoreneSlike[1]].classList.remove("is-flipped");
                 otvoreneSlike = [];
               }
-              if (cards[otvoreneSlike[0]].id === cards[otvoreneSlike[1]].id) {
-                cards[otvoreneSlike[0]].classList.add('uklonjena');
-                cards[otvoreneSlike[1]].classList.add('uklonjena');
+
+              if (document.querySelectorAll('.card:not(.uklonjena)').length === 0) {
+                let poruka = document.querySelector('.won-message');
+                poruka.style.display = 'block'; // Prikazuje poruku o pobedi
+                document.getElementById('restartContainer').style.display = 'block'; // Prikazuje dugme za restart
               }
-            }, 2000); //closes pics after 2s
+            }, 2000);
           }
         }
-      }
-      if (document.querySelectorAll('.card:not(.uklonjena)').length === 0) {
-        let poruka = document.getElementById('ispis');
-        poruka.innerHTML = 'You are winn.';
       }
     });
   });
@@ -77,3 +78,8 @@ function promesajNiz(array) {
     [array[i], array[j]] = [array[j], array[i]];
   }
 }
+
+// Restart logika
+document.getElementById("restartGame").addEventListener("click", function () {
+  location.reload(); // Osvježava stranicu za novi početak igre
+});
